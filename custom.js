f5bd3901b465f5c5e9cc44412dd2d578c47f17e3
Sunny Srinidhi <sunny.3mysore@gmail.com>
2017-01-18 12:01:11 +0530

    Created custom.js
    Initial commit.

---
+++ custom.js
@@ -0,0 +1,45 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires     = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+function deleteCookie(name) {
+    $.removeCookie(name, { path: '/' });
+}
+
+function getFormObj(formId) {
+    var formObj = {};
+    var inputs = $('#'+formId).serializeArray();
+    $.each(inputs, function (i, input) {
+        formObj[input.name] = input.value;
+    });
+    return formObj;
+}
+
+function populateDropDown(dropDownId, options) {
+
+    $.each(options, function (i, option) {
+        $('#' + dropDownId).append($('<option>', { 
+            value: option.value,
+            text : option.text 
+        }));
+    });
+}
